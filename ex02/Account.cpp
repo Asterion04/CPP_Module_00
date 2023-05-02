691703c298e3226d72f983b27e32676383bcbe1f
Ianis Conan Vautravers <ivautrav@c1r14s3.42lausanne.ch>
2023-05-02 18:32:23 +0200

    Added getters

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,11 +6,16 @@
 /*   By: ivautrav <ivautrav@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 08:50:03 by ivautrav          #+#    #+#             */
-/*   Updated: 2023/05/02 18:29:58 by ivautrav         ###   ########.fr       */
+/*   Updated: 2023/05/02 18:31:46 by ivautrav         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
 
 Account::Account(int initial_deposit)
 {
@@ -19,4 +24,24 @@ Account::Account(int initial_deposit)
 	this->_nbWithdrawals = 0;
 	this->_totalNbWithdrawals = 0;
 	this->_totalNbDeposits = 0;
+}
+
+int	Account::getNbAccounts(void)
+{
+	return _nbAccounts;
+}
+
+int	Account::getTotalAmount(void)
+{
+	return _totalAmount;	
+}
+
+int	Account::getNbDeposits(void)
+{
+	return _totalNbDeposits;	
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return _totalNbWithdrawals;
 }
